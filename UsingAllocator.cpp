e4311673b8fe2ceffdedb1deda5404ccfd7d9804
agentlans <agentlans@users.noreply.github.com>
2022-10-04 09:04:42 -0400

    Add files via upload

---
+++ UsingAllocator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <memory> // std::allocator
+
+// How to use an allocator instead of C++ new, delete.
+
+int main() {
+    // Create allocator instance
+    auto allo = std::allocator<int>();
+    int* arr = allo.allocate(10); // like new int[10];
+    // Do stuff with the array
+    for (int i = 0; i < 10; ++i) {
+        arr[i] = i;
+    }
+    for (int i = 0; i < 10; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    allo.deallocate(arr, 10); // like delete[] arr;
+        // 10 is a hint to the allocator object. Can be another number.
+}
